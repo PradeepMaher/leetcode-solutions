9f3bf604b38bf142db2f57d0792ab6e4a13e221e
Pradeep Maher <pradeepmaher1281@gmail.com>
2026-09-18 09:47:08 +0530

    Time: 0 ms (100%), Space: 11 MB (18.4%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n == 1) return nums[0];
+
+        vector<int> temp1, temp2;
+
+        // Case 1: Exclude the first house
+        // [1 ... n-1]
+        for(int i = 1; i < n; i++) {
+            temp1.push_back(nums[i]);
+        }
+
+        // Case 2: Exclude the last house
+        // [0 ... n-2]
+        for(int i = 0; i < n - 1; i++) {
+            temp2.push_back(nums[i]);
+        }
+
+        vector<int> dp1(n, -1);
+        vector<int> dp2(n, -1);
+
+        int case1 = solve(n - 2, temp1, dp1);
+        int case2 = solve(n - 2, temp2, dp2);
+
+        return max(case1, case2);
+    }
+
+    int solve(int i, vector<int>& nums, vector<int>& dp) {
+        if(i < 0) return 0;
+
+        if(i == 0) return nums[0];
+
+        if(dp[i] != -1) return dp[i];
+
+        int pick = nums[i] + solve(i - 2, nums, dp);
+        int notpick = solve(i - 1, nums, dp);
+
+        return dp[i] = max(pick, notpick);
+    }
+};
